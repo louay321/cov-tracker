95a77a1f07ae275d95ae74ebd03838db601e6056
louay nahdi <paladinx@live.fr>
2022-01-02 16:55:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect( ()=> {
+    refreshPage();
+  }, []);
+
+  const funcSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const refreshPage = () => {
+    setIsLoading(true);
+
+    var options = {
+      method: 'GET',
+      url: 'https://covid-19-data.p.rapidapi.com/country',
+      params: {name: search, format:'json'},
+      headers: {
+        'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+        'x-rapidapi-key': '7d617ac138mshf6344666c9fc1eep16f8fejsnfceb2920b5c7'
+      }
+    };
+
+    Axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+  
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Track covid changes in every country.</h1>
+      <div className="search-container">
+                    <input
+                        className="search"
+                        placeholder="Search"
+                        type="text"
+                        onChange={funcSearch}
+                    />
+                    <i className="fas fa-search" onClick={refreshPage}></i>
+                </div>
     </div>
   );
 }
